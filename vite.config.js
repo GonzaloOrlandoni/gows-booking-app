de1acc60624b8a2f13c1985091663e710863cb14
Gonzalo Orlandoni <gonzaloorlandoni1@gmail.com>
2026-04-30 20:55:07 -0300

    fix: adjust base path for gh-actions

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
-  return {
-    plugins: [
-      tailwindcss(),
-      react(),
-    ],
-    // Si estamos en GitHub Pages (usualmente detectado por una variable de entorno o simplemente para prod de GH)
-    // Pero para que funcione en VERCEL y LOCAL, el base debe ser '/'
-    // Solo usa '/gows-booking-app/' si vas a desplegar específicamente en GitHub Pages.
-    base: process.env.NODE_ENV === 'production' && !process.env.VERCEL ? '/gows-booking-app/' : '/',
-  }
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  // Si se compila en GitHub Actions, usa el subdirectorio. 
+  // En Local o Vercel, usa la raíz.
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/gows-booking-app/' : '/',
 })
